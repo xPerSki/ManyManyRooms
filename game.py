519aa068b38ab93a6ba761af1111d0e97daf6d1c
prsky <xperski@pm.me>
2024-06-22 17:53:37 +0200

    Rooms n reading

--- game.py
+++ game.py
@@ -1,4 +1,23 @@
 from random import randint
+import json
+
+
+class Room:
+    def __init__(self, num: int):
+        with open("rooms.json", 'r') as file:
+            content = file.read()
+
+        self.all_rooms = json.loads(content)
+        self.room_number = num
+
+    def get_enemies(self):
+        return self.all_rooms[self.room_number]["Enemies"]
+
+    def get_items(self):
+        return self.all_rooms[self.room_number]["Items"]
+
+    def get_description(self):
+        return self.all_rooms[self.room_number]["Description"]
 
 
 def roll_dice() -> int:
