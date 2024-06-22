8c4d413452a2f7054e27bb947d738cd0e2ea8e1c
prsky <xperski@pm.me>
2024-06-22 23:19:49 +0200

    battles, adding stats, finding items

--- game.py
+++ game.py
@@ -6,19 +6,57 @@ import os
 def roll_dice(n=6) -> int:
     return randint(1, n)
 
+
 class Room:
     def __init__(self, num: int):
         with open("rooms.json", 'r') as file:
             content = file.read()
 
         self.all_rooms = json.loads(content)
+        self.max_room = int(list(self.all_rooms.keys())[-1])
         self.room_number = num
 
+    def check_enemies(self) -> bool:
+        enemies = self.all_rooms[str(self.room_number)]["Enemies"]
+        if enemies != "This room is clear" and enemies != "None" and "escaped" not in enemies:
+            return True
+        return False
+
     def get_enemies(self) -> str:
+        plr = Player()
+        if self.check_enemies():
+            battle = plr.battle()
+            if battle[0]:
+                print("Won")
+                plr.change_stat("health", -battle[1])
+            else:
+                print("Lost")
+                plr.write_data("0,0,0,0")
+
         return self.all_rooms[str(self.room_number)]["Enemies"]
 
+    def check_items(self):
+        items = self.all_rooms[str(self.room_number)]["Items"]
+        if items != "None":
+            return True
+        return False
+
     def get_items(self) -> str:
-        return self.all_rooms[str(self.room_number)]["Items"]
+        items = self.all_rooms[str(self.room_number)]["Items"]
+        plr = Player()
+        if items != "None":
+            stat_changes = {
+                "health": ("health", 5),
+                "dagger": ("damage", 3),
+                "map": ("luck", 2),
+                "key": ("keys", 1),
+                "coin": ("luck", 2),
+                "herb": ("health", 3)
+            }
+            for item, (stat, value) in stat_changes.items():
+                if item in items.lower():
+                    plr.change_stat(stat, value)
+        return items
 
     def get_description(self) -> str:
         return self.all_rooms[str(self.room_number)]["Description"]
@@ -49,14 +87,47 @@ class Player:
         health, damage, luck = roll_dice() * 2 + 6, roll_dice() * 2 + 3, roll_dice()
         self.write_data(f"{health},{damage},{luck},0")
 
-    def get_health(self):
-        return self.read_data().split(',')[0]
+    def change_stat(self, stat: str, n: int) -> None:
+        stats = {
+            "health": self.get_health(),
+            "damage": self.get_damage(),
+            "luck": self.get_luck(),
+            "keys": self.get_keys()
+        }
+        old = stats[stat]
+        new_values = {k: v for k, v in stats.items()}
+        new_values[stat] = old + n
+        self.write_data(f"{new_values['health']},{new_values['damage']},{new_values['luck']},{new_values['keys']}")
 
-    def get_damage(self):
-        return self.read_data().split(',')[1]
+    def get_health(self) -> int:
+        return int(self.read_data().split(',')[0])
 
-    def get_luck(self):
-        return self.read_data().split(',')[2]
+    def get_damage(self) -> int:
+        return int(self.read_data().split(',')[1])
 
-    def get_keys(self):
-        return self.read_data().split(',')[3]
+    def get_luck(self) -> int:
+        return int(self.read_data().split(',')[2])
+
+    def get_keys(self) -> int:
+        return int(self.read_data().split(',')[3])
+
+    def battle(self):
+        print("Fight")
+        user_health = self.get_health()
+        user_damage = self.get_damage()
+        user_luck = self.get_luck()
+
+        enemy_health, enemy_damage = randint(4, 12), randint(1, 8)
+        user_luck *= 3
+
+        damage_received = 0
+        while True:
+            crit = True if randint(1, 100) <= user_luck else False
+            enemy_health -= user_damage * 2 if crit else user_damage
+            if enemy_health <= 0:
+                return True, damage_received
+
+            user_health -= enemy_damage
+            damage_received += enemy_damage
+            if user_health <= 0:
+                return False, None
