16b089391c8aa62c4c386a05c8aac3426d6485e5
prsky <xperski@pm.me>
2024-06-22 16:20:02 +0200

    update index site

---
+++ game.py
@@ -0,0 +1,5 @@
+from random import randint
+
+
+def roll_dice() -> int:
+    return randint(1, 6)
--- server.py
+++ server.py
@@ -1,3 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+import game
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+
+@app.route("/game")
+def game():
+    game_variables = {
+        "room": 0,
+        "health": 0,
+        "damage": 0,
+        "luck": 0,
+    }
+    return render_template("game.html", **game_variables)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
